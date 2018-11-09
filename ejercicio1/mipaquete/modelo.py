546bebe4b0bfe2649edc33071050cf1b9e830d93
Jose Pullaguari <jepullaguari@utpl.edu.ec>
2018-11-09 12:19:17 -0500

    Subiendo segundo comit de la leccion

--- ejercicio1/mipaquete/modelo.py
+++ ejercicio1/mipaquete/modelo.py
@@ -32,11 +32,103 @@ class Empleado(object):#Creamos la clase padre Empleado}
 	def obtener_comision_fija(self):
 		return self.comision
 
-	#Metodos de obtener datos
+	#Metodo de presentar datos datos
 	def presentar_datos(self):
-		cadena = "Informacion de %s %s\n\t cedula:%s"%(self.obtener_nombre(),self.apellido,self.obtener_cedula())
+		cadena = "Informacion de: %s %s\n\t cedula:%s\n"%(self.obtener_nombre(),self.apellido,self.obtener_cedula())
 		return cadena
 
+#Creamos la clase Empleado por Horas
+class EmpleadoPorHoras(Empleado):
+	#Constructor de la clase
+	def __init__(self):
+		super(EmpleadoPorHoras,self).__init__()#Heredamos con el super de la clase padre Empleado
+		self.numero_horas = 0
+		self.valor_hora = 0
 
+	#Metodos de agregar
+	def agregar_numero_horas(self,numero):
+		self.numero_horas = numero
 
+	def agregar_valor_hora(self,valor):
+		self.valor_hora = valor
 
+	#Metodos de obtener
+	def obtener_numero_horas(self):
+		return self.numero_horas
+
+	def obtener_valor_horas(self):
+		return self.valor_hora
+
+	#Metodo para calcular el sueldo final
+	def calcular_sueldo_final(self):
+		return (self.numero_horas * self.valor_hora) + self.comision_fija
+	
+	#Metodo de presentar datos
+	def presentar_datos(self):
+		cadena = "%s\n Numero horas:%s\n Valor hora:%s\n Sueldo Final: %s"%(super(EmpleadoPorHoras,self).presentar_datos(),
+																			self.obtener_numero_horas(),self.obtener_valor_horas(),self.calcular_sueldo_final())
+		return cadena
+
+#Creamos la clase Empleado Fijo
+class EmpleadoFijo(Empleado):
+	#constructor de la clase
+	def __init__(self):
+		super(EmpleadoFijo,self).__init__()#Heredamos de la clase padre Empleado
+		self.sueldo_fijo = 0
+		self.descuento_seguro = 0
+
+	#Metodos de agregar
+	def agregar_sueldo_fijo(self,fijo):
+		self.sueldo_fijo = fijo
+
+	def agregar_descuento_fijo(self,descuento):
+		self.descuento_seguro = descuento
+
+	#Metodos de obtener
+	def obtener_sueldo_fijo(self):
+		return self.sueldo_fijo
+
+	def obtener_descuento_seguro(self):
+		return self.descuento_seguro
+
+	#Metodo para calcular el sueldo final
+	def calcular_sueldo_final(self):
+		return (self.sueldo_fijo - self.descuento_seguro) + self.comision_fija
+
+	#Metodo para presentar datos
+	def presentar_datos(self):
+		cadena = "%s\n Sueldo fijo: %s\n Descuento Seguro: %s\n Sueldo Final: %s"%(super(EmpleadoFijo,self).presentar_datos(),
+																					self.obtener_sueldo_fijo(),self.obtener_descuento_seguro(),self.calcular_sueldo_final())
+		return cadena
+
+#Creamos la clase Empleado por Semana
+class EmpleadoPorSemana(Empleado):
+	#constructor de la clase
+	def __init__(self):
+		super(EmpleadoPorSemana,self).__init__()#Heredamos de la clase padre Empleado
+		self.numero_semanas = 0
+		self.valor_semanal = 0
+
+	#Metodos de agregar
+	def agregar_numero_semanas(self,numero):
+		self.numero_semanas = numero
+
+	def agregar_valor_semanal(self,valor_semana):
+		self.valor_semanal = valor_semana
+
+	#Metodos de obtener
+	def obtener_numero_semanas(self):
+		return numero_semanas
+
+	def obtener_valor_semanal(self):
+		return valor_semanal
+
+	#Metodo de calcular sueldo final
+	def calcular_sueldo_final(self):
+		return (self.numero_semanas * self.valor_semanal) + self.comision_fija
+
+	#Metodo de presentar datos
+	def presentar_datos(self):
+		cadena = "%s\n Numero Semanas: %s\n Valor semanal: %s\n Sueldo Final: %s"%(super(EmpleadoPorSemana,self).presentar_datos(),
+																		            self.obtener_numero_semanas(),self.obtener_valor_semanal(),self.calcular_sueldo_final())
+		return cadena
--- ejercicio1/run.py
+++ ejercicio1/run.py
@@ -1,6 +1,29 @@
+#Importamos la clase modelo
 from mipaquete.modelo import *
-e=Empleado()
+#Creamos el objeto e tipo Empleado
+e = Empleado()
 e.agregar_nombre("Luis")
 e.agregar_apellido("Benitez")
-e.agregar_cedula("110101010")
-print(e.presentar_datos())
+e.agregar_cedula("1175948392")
+print(e.presentar_datos())#presentamos los datos
+
+#Creamos el objeto e1 de tipo Empleado
+e1 = EmpleadoPorHoras()
+nombre = input("Ingrese su nombre:\n")
+e1.agregar_nombre(nombre)
+e1.agregar_apellido("Andrade")
+e1.cedula = "1174859204"
+e1.agregar_valor_hora(20.2)
+e1.agregar_numero_horas(15)
+print(e1.presentar_datos())#presentamos los datos
+
+#Creamos el objeto e2 de tipo Empleado
+e2 = EmpleadoFijo()
+e2.agregar_sueldo_fijo(300.2)
+e2.descuento_seguro = (10)#Es el porcentaje
+comision = input("\nIngrese comision:")
+comision = float(comision)
+e2.agregar_comision_fija(comision)
+e2.nombre = "Ana"
+e2.apellido ="Diaz"
+print(e2.presentar_datos())#presentamos los datos
